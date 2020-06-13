bbd69a18fb58f64277bcfd2a250211fbbd6d26a7
Joliver Estampador <joliverestampador@gmail.com>
2020-06-13 11:29:19 +0800

    UI polishing

--- nuxt-first-project/pages/index.vue
+++ nuxt-first-project/pages/index.vue
@@ -1,11 +1,112 @@
 <template>
-  
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+    <section class="featured-posts">
+      <nuxt-link :to="`/posts/${1}`">
+        <article class="post-preview">
+          <div class="post-thumbnail"
+               style="background-image: url('https://thyblackman.com/wp-content/uploads/2018/11/TECH.jpg')"></div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+      </nuxt-link>
+      <nuxt-link :to="`/posts/${2}`">
+        <article class="post-preview">
+          <div class="post-thumbnail"
+               style="background-image: url('https://hypertechx.com/wp-content/uploads/2017/10/gettyimages-186450097.jpg')"></div>
+          <div class="post-content">
+            <h1>Post Title</h1>
+            <p>Preview Text</p>
+          </div>
+        </article>
+      </nuxt-link>
+    </section>
+  </div>
 </template>
 
 <script>
 
 </script>
 
-<style>
+<style scoped>
+.intro {
+  max-height: 300px;
+  margin-bottom: 50px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
 
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  margin-right: 10px;
+}
+
+a {
+  width: 50%;
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    /* width: 400px; */
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
 </style>
