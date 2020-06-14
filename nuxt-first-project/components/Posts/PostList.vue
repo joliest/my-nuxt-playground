2a41702c5ece614c4f3b6de0b2a6f99c0db067ce
Joliver Estampador <joliverestampador@gmail.com>
2020-06-14 20:28:50 +0800

    Create Post and Edit Post

--- nuxt-first-project/components/Posts/PostList.vue
+++ nuxt-first-project/components/Posts/PostList.vue
@@ -2,11 +2,13 @@
     <section class="post-list">
         <PostPreview 
             id="1"
+            :isAdmin="isAdmin"
             thumbnails="https://thyblackman.com/wp-content/uploads/2018/11/TECH.jpg"
             title="Hello there"
             previewText="This is my first post" />
         <PostPreview 
             id="2"
+            :isAdmin="isAdmin"
             thumbnails="https://thyblackman.com/wp-content/uploads/2018/11/TECH.jpg"
             title="Hello there - second time"
             previewText="This is my second post" />
@@ -19,6 +21,12 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 }
 </script>
---
+++ nuxt-first-project/pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    components: {
+        AdminPostForm,
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Joli',
+                title: 'My awesome post',
+                content: 'Super amazing',
+                thumbnailLink: 'https://thyblackman.com/wp-content/uploads/2018/11/TECH.jpg'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
