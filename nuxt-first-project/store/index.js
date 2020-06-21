d21311d2a4bc6c0b21f3f3ca7cae1956e905268e
Joliver Estampador <joliverestampador@gmail.com>
2020-06-21 15:39:47 +0800

    Add a condition to run localStorage in client environment only

--- nuxt-first-project/store/index.js
+++ nuxt-first-project/store/index.js
@@ -136,8 +136,10 @@ const createStore = () => {
                 commit('clearToken')
                 Cookie.remove('jwt')
                 Cookie.remove('expirationDate')
-                localStorage.removeItem('token')
-                localStorage.removeItem('tokenExpiration')
+                if (process.client) {
+                    localStorage.removeItem('token')
+                    localStorage.removeItem('tokenExpiration')
+                }
             }
         },
         getters: {
