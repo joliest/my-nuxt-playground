4058293af57f1e63e00e5fff8413692172a9a013
Joliver Estampador <joliverestampador@gmail.com>
2020-06-21 13:43:33 +0800

    Adding a token

--- nuxt-first-project/store/index.js
+++ nuxt-first-project/store/index.js
@@ -3,7 +3,8 @@ import Vuex from 'vuex';
 const createStore = () => {
     return new Vuex.Store({
         state: {
-            loadedPosts: []
+            loadedPosts: [],
+            token: null
         },
         mutations: {
             setPosts(state, posts) {
@@ -17,6 +18,9 @@ const createStore = () => {
                     post.id === editedPost.id
                 );
                 state.loadedPosts[postIndex] = editedPost;
+            },
+            setToken(state, token) {
+                state.token = token
             }
         },
         actions: {
@@ -53,6 +57,25 @@ const createStore = () => {
                     .then(res => {
                         // updating vuex store to make it sync
                         commit('editPost', editedPost)
+                    })
+                    .catch(e => console.log(e))
+            },
+            authenticateUser({commit}, authData) {
+                let authUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.fbAPIKey}`
+      
+                if (!authData.isLogin) {
+                    authUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.fbAPIKey}`
+                }
+
+                // return a Promise() to access then()
+                return this.$axios
+                    .$post(authUrl, {
+                        email: authData.email,
+                        password: authData.password,
+                        returnSecureToken: true
+                    })
+                    .then(result => {
+                        commit('setToken', result.idToken)
                     })
                     .catch(e => console.log(e))
             }
