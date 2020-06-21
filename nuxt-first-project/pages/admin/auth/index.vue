9c12d8c2240328812a5e99d5056ee39ee0cd345b
Joliver Estampador <joliverestampador@gmail.com>
2020-06-21 13:30:54 +0800

    Adding sign in functionality

--- nuxt-first-project/pages/admin/auth/index.vue
+++ nuxt-first-project/pages/admin/auth/index.vue
@@ -28,8 +28,14 @@ export default {
   },
   methods: {
     onSubmit() {
+      let authUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.fbAPIKey}`
+      
+      if (!this.isLogin) {
+        authUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.fbAPIKey}`
+      }
+
       this.$axios
-        .$post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.fbAPIKey}`, {
+        .$post(authUrl, {
           email: this.email,
           password: this.password,
           returnSecureToken: true
